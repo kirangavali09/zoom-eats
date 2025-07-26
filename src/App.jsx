4306e3a5a8938b373d7dae862da930c80aab4ead
Kiran <kiran@codebox.co.in>
2025-07-26 19:17:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <h1>ZoomEats</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
