0af7fc58fd5584151202d331a3fd5bea2e7715ea
kiran905 <kirangavali939@gmail.com>
2025-07-29 22:54:42 +0530

    Hamberger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import Logo from './../assets/logo.svg';
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
 
     return (
-        <div className="w-full h-12 sm:h-20 px-4 sm:px-12 bg-zinc-100 lg:px-32 pt-2 flex justify-between shadow-lg">
-            <div className="w-32 sm:w-56">
+        <div className="w-full h-16 sm:h-20 px-4 sm:px-12 bg-zinc-100 lg:px-32 pt-2 flex justify-between shadow-lg">
+            <div className="w-36 sm:w-56">
                 <img src={Logo} className='w-full h-full object-cover'/>
             </div>
             <div className='flex gap-4 items-center max-sm:hidden'>
@@ -12,6 +13,9 @@ const Navbar = () => {
                 <h4 className='font-bold text-xl uppercase text-zinc-900 cursor-pointer'>About</h4>
                 <h4 className='font-bold text-xl uppercase text-zinc-900 cursor-pointer'>Contact Us</h4>
                 <h4 className='font-bold text-xl uppercase text-zinc-900 cursor-pointer'>Cart</h4>
+            </div>
+            <div className='sm:hidden flex items-center text-2xl'>
+                <RxHamburgerMenu />
             </div>
         </div>
     )
