4cd29ec779d3110c9f95622d5e3dae81269da532
kirangavali905 <kiran@codebox.co.in>
2025-08-06 19:46:25 +0530

    restaurantdetail

--- src/components/RestaurantDetail.jsx
+++ src/components/RestaurantDetail.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const RestaurantDetail = () => {
     let { restaurantId } = useParams();
-    console.log(restaurantId);
+    const [restaurantData, setRestaurantData] = useState([])
+    // console.log(restaurantId);
+
+    const getRestaurantData = async () => {
+        let restaurantData = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.1264801&lng=73.0110096&restaurantId=${restaurantId}&catalog_qa=undefined&submitAction=ENTER`)
+        let restaurantJson = await restaurantData.json();
+
+        console.log(restaurantJson);
+    }
+
+    useEffect(() => {
+        getRestaurantData();
+    }, [])
+
     return (
         <div>
             Hello , Welcome to restro page.
