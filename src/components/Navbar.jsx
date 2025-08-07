8ca1fb0e369df106a97dbdc26fd9b5892afd6591
kirangavali905 <kiran@codebox.co.in>
2025-08-07 18:23:36 +0530

    Restaurant Detail

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import { useState } from 'react';
 import Logo from './../assets/logo.svg';
 import { RxHamburgerMenu } from "react-icons/rx";
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(0);
 
     return (
         <div className="w-full h-16 sm:h-20 px-4 sm:px-12 bg-zinc-100 lg:px-32 pt-2 flex justify-between shadow-lg">
-            <div className="w-36 sm:w-56">
+            <Link to="/" className="w-36 sm:w-56">
                 <img src={Logo} className='w-full h-full object-cover'/>
-            </div>
+            </Link>
             <div className='flex gap-4 items-center max-sm:hidden'>
-                <h4 className='font-bold text-xl uppercase text-zinc-900 cursor-pointer'>Home</h4>
-                <h4 className='font-bold text-xl uppercase text-zinc-900 cursor-pointer'>About</h4>
-                <h4 className='font-bold text-xl uppercase text-zinc-900 cursor-pointer'>Contact Us</h4>
-                <h4 className='font-bold text-xl uppercase text-zinc-900 cursor-pointer'>Cart</h4>
-                <h4 className='px-3 py-1 text-xl bg-slate-400 cursor-pointer text-white rounded-md' onClick={() => setIsLoggedIn(!isLoggedIn)}>{ isLoggedIn ? "Logout" : "Login" }</h4>
+                <NavLink to="/" className='font-bold text-xl uppercase text-zinc-900 cursor-pointer'>Home</NavLink>
+                <NavLink className='font-bold text-xl uppercase text-zinc-900 cursor-pointer'>About</NavLink>
+                <NavLink className='font-bold text-xl uppercase text-zinc-900 cursor-pointer'>Contact Us</NavLink>
+                <NavLink className='font-bold text-xl uppercase text-zinc-900 cursor-pointer'>Cart</NavLink>
+                <NavLink className='px-3 py-1 text-xl bg-slate-400 cursor-pointer text-white rounded-md' onClick={() => setIsLoggedIn(!isLoggedIn)}>{ isLoggedIn ? "Logout" : "Login" }</NavLink>
             </div>
             <div className='sm:hidden flex items-center text-2xl'>
                 <RxHamburgerMenu />
--- src/components/RestaurantDetail.jsx
+++ src/components/RestaurantDetail.jsx
@@ -1,25 +1,63 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { RESTAURANT_DETAIL_API_URL } from "../utils/constants";
+import Shimmer from "./Shimmer";
+import { FaStar } from "react-icons/fa";
 
 const RestaurantDetail = () => {
-    let { restaurantId } = useParams();
-    const [restaurantData, setRestaurantData] = useState([])
-    // console.log(restaurantId);
+    const { restaurantId } = useParams();
+    const [ restaurantData, setRestaurantData] = useState([]);
 
-    const getRestaurantData = async () => {
-        let restaurantData = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.1264801&lng=73.0110096&restaurantId=${restaurantId}&catalog_qa=undefined&submitAction=ENTER`)
+    const fetchRestaurantData = async () => {
+        let restaurantData = await fetch(RESTAURANT_DETAIL_API_URL + restaurantId)
         let restaurantJson = await restaurantData.json();
 
-        console.log(restaurantJson);
+        setRestaurantData(restaurantJson); 
     }
 
     useEffect(() => {
-        getRestaurantData();
+        fetchRestaurantData();
     }, [])
 
+    if (restaurantData.length == 0) return  <Shimmer />;
+
+    console.log(restaurantData?.data?.cards[2]?.card?.card?.info);
+    let {avgRating, totalRatingsString, costForTwoMessage, cuisines, areaName, sla} = restaurantData?.data?.cards[2]?.card?.card?.info;
+
+
     return (
-        <div>
-            Hello , Welcome to restro page.
+        <div className="w-2/5 mx-auto my-10">
+            <div className="restaurant-name text-4xl font-bold p-6">
+                {restaurantData?.data?.cards[0]?.card?.card?.text}
+            </div>
+            <div className="w-full bg-linear-to-t from-gray-300 via-gray-200 to-white pb-4 pt-1 rounded-3xl">
+                <div className="w-[96%] mx-auto bg-white rounded-2xl p-6 border-1 border-gray-300 shadow-md">
+                    <div className="flex gap-2 items-center mb-2">
+                        <FaStar className="bg-green-600 text-white text-xl rounded-full p-1" />
+                        <span className="font-semibold">{avgRating}</span>
+                        <span className="font-semibold">({totalRatingsString})</span>
+                        <span className="list-inside list-disc">
+                            <li className="font-semibold ml-2">{ costForTwoMessage }</li>
+                        </span>
+                    </div>
+                    <div className="cousins text-sm font-semibold text-orange-600 cursor-pointer underline">
+                        {cuisines.join(", ")}
+                    </div>
+
+                    <ol className="relative border-s border-gray-400 my-2">                 
+                        <li class="mb-4 ms-4">
+                            <div class="absolute w-2 h-2 bg-gray-300 rounded-full -left-1 border border-gray-300"></div>
+                            <div class="mb-1 leading-none text-sm font-semibold">Outlet
+                               <span className="ms-2 text-sm font-normal text-gray-600">{areaName}</span>
+                            </div>
+                        </li>
+                        <li class=" ms-4">
+                            <div class="absolute w-2 h-2 bottom-0 bg-gray-300 rounded-full mt-1.5 -start-1 border border-gray-300"></div>
+                            <div class="text-sm font-semibold">{sla.slaString }</div>
+                        </li>
+                    </ol>
+                </div>
+            </div>
         </div>
     )
 }
