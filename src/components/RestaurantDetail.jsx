2530cee655dd328543755d383dc40dd32fbae605
kirangavali905 <kiran@codebox.co.in>
2025-08-08 11:46:06 +0530

    Offers section

--- src/components/RestaurantDetail.jsx
+++ src/components/RestaurantDetail.jsx
@@ -8,6 +8,7 @@ import { FreeMode, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
+import { OFFERS_MEDIA_URL } from "../utils/constants"
 
 const RestaurantDetail = () => {
     const { restaurantId } = useParams();
@@ -26,7 +27,7 @@ const RestaurantDetail = () => {
 
     if (restaurantData.length == 0) return  <Shimmer />;
 
-    console.log(restaurantData?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.offers);
+    const offers = restaurantData?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.offers;
     return (
         <div className="w-2/5 mx-auto my-10">
             <RestaurantDetailHero name={restaurantData?.data?.cards[0]?.card?.card?.text} restaurantData={restaurantData?.data?.cards[2]?.card?.card?.info} />
@@ -36,115 +37,26 @@ const RestaurantDetail = () => {
                     Deals for you
                 </div>
                 <Swiper
-                    slidesPerView={3}
-                    spaceBetween={30}
+                    slidesPerView={2.5}
+                    spaceBetween={20}
                     freeMode={true}
-                    pagination={{
-                    clickable: true,
-                    }}
-                    modules={[FreeMode, Pagination]}
-                    className="mySwiper"
+                    modules={[FreeMode,]}
                 >
-                    <SwiperSlide>
-                        <div className="p-4 border-2 border-gray-400 rounded-xl">
-                            <div>
-                                Image
-                            </div>
-                            <div>
-                                content
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="p-4 border-2 border-gray-400 rounded-xl">
-                            <div>
-                                Image
-                            </div>
-                            <div>
-                                content
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="p-4 border-2 border-gray-400 rounded-xl">
-                            <div>
-                                Image
-                            </div>
-                            <div>
-                                content
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="p-4 border-2 border-gray-400 rounded-xl">
-                            <div>
-                                Image
-                            </div>
-                            <div>
-                                content
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="p-4 border-2 border-gray-400 rounded-xl">
-                            <div>
-                                Image
-                            </div>
-                            <div>
-                                content
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="p-4 border-2 border-gray-400 rounded-xl">
-                            <div>
-                                Image
-                            </div>
-                            <div>
-                                content
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="p-4 border-2 border-gray-400 rounded-xl">
-                            <div>
-                                Image
-                            </div>
-                            <div>
-                                content
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="p-4 border-2 border-gray-400 rounded-xl">
-                            <div>
-                                Image
-                            </div>
-                            <div>
-                                content
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="p-4 border-2 border-gray-400 rounded-xl">
-                            <div>
-                                Image
-                            </div>
-                            <div>
-                                content
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="p-4 border-2 border-gray-400 rounded-xl">
-                            <div>
-                                Image
-                            </div>
-                            <div>
-                                content
-                            </div>
-                        </div>
-                    </SwiperSlide>
+                    {
+                        offers.map((offer) => (
+                            <SwiperSlide >
+                                <div className="p-4 border-1 border-gray-400 rounded-xl flex gap-3">
+                                    <div className="w-10 h-10">
+                                        <img src={OFFERS_MEDIA_URL+ offer?.info?.offerLogo} className="w-full h-full object-cover" />
+                                    </div>
+                                    <div>
+                                        <div className="font-semibold text-md ">{offer?.info?.header}</div>
+                                        <div className="text-sm text-gray-400 font-semibold">{offer?.info?.primaryDescription || offer?.info?.description}</div>
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                        ))
+                    }
                     
                 </Swiper>
             </div>
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -4,3 +4,5 @@ export const IMAGE_BASE_URL = "https://media-assets.swiggy.com/swiggy/image/uplo
 export const RESTAURANT_API_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.1760819&lng=73.02288949999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const RESTAURANT_DETAIL_API_URL ="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.1264801&lng=73.0110096&restaurantId=";
+
+export const OFFERS_MEDIA_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_96,h_96/";
